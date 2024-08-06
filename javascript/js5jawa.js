8166ca763b9ce98340feccaadc3987a5c01f0187
Xingakapas <altaf2104@student.abudzar.sch.id>
2024-08-06 11:08:39 +0700

    latuhan

---
+++ javascript/js5jawa.js
@@ -0,0 +1,35 @@
+let number = 5;
+
+if (number < 0) {
+  console.log("Negative");
+} else if (number === 0) {
+  console.log("Zero");
+} else if (number > 0) {
+  console.log("Positive");
+} else {
+  console.log("Unknown");
+}
+
+let hari = "Minggu";
+
+switch (hari.toLowerCase()) {
+  case "sabtu":
+  case "minggu":
+    console.log("Weekend");
+    break;
+  default:
+    console.log("Weekday");
+}
+
+for (let i = 1; i <= 15; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
