239ed30e74b6dfbffe296b25348e4b584d461878
Xingakapas <altaf2104@student.abudzar.sch.id>
2024-10-09 11:23:56 +0700

    tugas16

--- javascript/tugas16/soal1.js
+++ javascript/tugas16/soal1.js
@@ -5,3 +5,10 @@ myDiv.style.color = 'red';
 // Mengambil elemen pertama dengan kelas "coba" dan mengubah kontennya
 const firstCoba = document.querySelector('.coba');
 firstCoba.textContent = 'Elemen pertama yang diubah';
+
+let id document.getElementById("id");
+let kelas = document.getElementsByClassName("coba");
+let tag = document.getElementsByTagName("p");
+console.log(id)
+console.log(kelas [0])
+console.log(tag [0])
